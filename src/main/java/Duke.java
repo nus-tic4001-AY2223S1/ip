830bb102ba8970b6d95deeccb06450c8196a9018
roshankumar1991 <roshan.kumar@outlook.sg>
2022-08-19 00:46:46 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 public class Duke {
+    public static void echo() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        if (line.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+        else {
+            System.out.println(line + "\n");
+            echo();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(""
-                + "Hello! I'm Duke\n"
-                + "What can I do for you?\n\n"
-                + "Bye. Hope to see you again soon!");
+        String greet = "Hello! I'm Duke\n" + "What can I do for you?\n";
+        System.out.println(greet);
+        echo();
     }
 }
