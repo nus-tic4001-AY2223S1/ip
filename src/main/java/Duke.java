1cf57f3c50289628d2fece4e3570d0e23fdbef88
roshankumar1991 <roshan.kumar@outlook.sg>
2022-08-19 10:19:32 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,45 @@ public class Duke {
     public static int seq = 1;
     public static Task[] list = new Task[100];
 
+    public static void sort(String t) {
+        if (t.contains("todo")) {
+            String description = t.substring(5);
+            Task[] print = Arrays.copyOf(list,count);   //To prompt an error if task is added previously
+            for (Task p : print) {
+                if (p.description.equals(description)) {
+                    System.out.println("Error: Task has already been added previously\n");
+                    echo();
+                }
+            }
+            list[count] = new Todo(description);
+        }
+        else if (t.contains("deadline")) {
+            int n = t.indexOf('/');
+            String description = t.substring(9, n-1);
+            String by = t.substring(n+4);
+            Task[] print = Arrays.copyOf(list,count);   //To prompt an error if task is added previously
+            for (Task p : print) {
+                if (p.description.equals(description)) {
+                    System.out.println("Error: Task has already been added previously\n");
+                    echo();
+                }
+            }
+            list[count] = new Deadline(description, by);
+        }
+        else if (t.contains("event")) {
+            int n = t.indexOf('/');
+            String description = t.substring(6, n-1);
+            String at = t.substring(n+4);
+            Task[] print = Arrays.copyOf(list,count);   //To prompt an error if task is added previously
+            for (Task p : print) {
+                if (p.description.equals(description)) {
+                    System.out.println("Error: Task has already been added previously\n");
+                    echo();
+                }
+            }
+            list[count] = new Event(description, at);
+        }
+    }
     public static void echo() {
         String line;
         Scanner in = new Scanner(System.in);
@@ -12,6 +51,7 @@ public class Duke {
 
         if (line.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
+            System.exit(0);
         }
         else if (line.equals("list")) {
                 System.out.println("Here are the tasks in your list:\n");
@@ -67,9 +107,11 @@ public class Duke {
             echo();
         }
         else {
-            System.out.println("added: " + line + "\n");
-            list[count] = new Task(line);
+            sort(line);
+            System.out.println("Got it. I've added this task:");
+            System.out.println(list[count]);
             count++;
+            System.out.println("Now you have " + count + " tasks in the list.\n");
             echo();
         }
     }
