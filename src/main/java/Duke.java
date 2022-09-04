7feddbc84a1f1a6ccf26ab3132bcdb608ef465df
BADASSENGINEER <b.rabang@kajima.com>
2022-09-04 17:25:11 +0800

    Level-5.1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,7 +12,7 @@ public class Duke {
 
         System.out.println();
 
-        if ((line.equals("bye")) || (line.equals("list")) || (line.startsWith("mark")) || line.startsWith("unmark")) {
+        if ((line.startsWith("bye")) || (line.startsWith("list")) || (line.startsWith("mark")) || line.startsWith("unmark")) {
             operateTasks(line);
         } else if ((line.startsWith("todo")) || (line.startsWith("deadline")) || (line.startsWith("event"))) {
             addTask(line);
@@ -33,7 +33,7 @@ public class Duke {
                 break;
             case "list":
                 if (taskList.isEmpty()) {
-                    System.out.println("    The list is empty!");
+                    throw new DukeException("    The list is empty!");
                 } else {
                     System.out.println("    Here are the tasks in your list:");
                     for (int i = 0; i < taskList.size(); i++) {
@@ -42,21 +42,37 @@ public class Duke {
                 }
                 break;
             case "mark":
-                int markTaskIndex = Integer.parseInt(s.substring(5)) - 1;
+                try {
+                    int markTaskIndex = Integer.parseInt(s.substring(5)) - 1;
 
-                taskList.get(markTaskIndex).setStatusIcon("mark");
+                    taskList.get(markTaskIndex).setStatusIcon("mark");
 
-                System.out.println("    Nice! I've marked this task as done:");
-                System.out.println("      " + taskList.get(markTaskIndex));
-                break;
+                    System.out.println("    Nice! I've marked this task as done:");
+                    System.out.println("      " + taskList.get(markTaskIndex));
+                    break;
+                } catch (StringIndexOutOfBoundsException e) {
+                    throw new DukeException("\u2639" + " Check the Duke basic input commands!!! Correct input format for \'mark\' keyword must be provided.");
+                } catch (IndexOutOfBoundsException e) {
+                    throw new DukeException("\u2639" + " OOPS!!! It's either the task's list is empty or the index entered is out of bound.");
+                } catch (NumberFormatException e) {
+                    throw new DukeException("\u2639" + " Check the Duke basic input commands!!! \'mark\' keyword must be succeeded with a positive integer.");
+                }
             case "unmark":
-                int unmarkTaskIndex = Integer.parseInt(s.substring(7)) - 1;
+                try {
+                    int unmarkTaskIndex = Integer.parseInt(s.substring(7)) - 1;
 
-                taskList.get(unmarkTaskIndex).setStatusIcon("unmark");
+                    taskList.get(unmarkTaskIndex).setStatusIcon("unmark");
 
-                System.out.println("    OK, I've marked this task as not done yet:");
-                System.out.println("      " + taskList.get(unmarkTaskIndex));
-                break;
+                    System.out.println("    OK, I've marked this task as not done yet:");
+                    System.out.println("      " + taskList.get(unmarkTaskIndex));
+                    break;
+                } catch (StringIndexOutOfBoundsException e) {
+                    throw new DukeException("\u2639" + " Check the Duke basic input commands!!! Correct input format for \'unmark\' keyword must be provided.");
+                } catch (IndexOutOfBoundsException e) {
+                    throw new DukeException("\u2639" + " OOPS!!! It's either the task's list is empty or the index entered is out of bound.");
+                } catch (NumberFormatException e) {
+                    throw new DukeException("\u2639" + " Check the Duke basic input commands!!! \'unmark\' keyword must be succeeded with a positive integer.");
+                }
         }
     }
 
